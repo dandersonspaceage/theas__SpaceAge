9bad4dc57e3a04b27acbdeffb8c0563a85d541da
DavidRueter <drueter@opsstream.com>
2021-12-17 06:54:59 -0800

    WIP

--- saltd/sched_App.vue
+++ saltd/sched_App.vue
@@ -147,7 +147,44 @@
       </b-row>
 
     <b-container>
+
+    <!-- template for the modal component -->
+    <script type="text/x-template" id="modal-template">
+      <transition name="modal">
+        <div class="modal-mask">
+          <div class="modal-wrapper">
+            <div class="modal-container">
+
+              <div class="modal-header">
+                <slot name="header">
+                  default header
+                </slot>
+              </div>
+
+              <div class="modal-body">
+                <slot name="body">
+                  default body
+                </slot>
+              </div>
+
+              <div class="modal-footer">
+                <slot name="footer">
+                  default footer
+                  <button class="modal-default-button" @click="$emit('close')">
+                    OK
+                  </button>
+                </slot>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </script>
+
+
+    <modal v-if="showModal" @close="showModal = false">
   </div>
+
 </template>
 
 <script>
@@ -167,7 +204,7 @@
 
         overlayVisible: false,
 
-        currentTabNumber: 0,
+        showModal: false,
 
         // Dynamic data will be fetched asynchronously
         data_WOs: [],
@@ -202,8 +239,9 @@
 
       testMe: function() {
         let thatVue = this;
+        thatVue.showModal = true;
 
-        thatVue.$th.showModal('Here is a fake error')
+        //thatVue.$th.showModal('Here is a fake error')
       },
 
       switchWOList: function() {
