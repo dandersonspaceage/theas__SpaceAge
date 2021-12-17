4a84b0b168bf0fdc46be432c36b646199bc60e4e
DavidRueter <drueter@opsstream.com>
2021-12-17 07:00:53 -0800

    WIP

--- saltd/sched_App.vue
+++ saltd/sched_App.vue
@@ -146,11 +146,12 @@
 
       </b-row>
 
-    <b-container>
-
   <b-modal id="modal-1" title="BootstrapVue">
     <p class="my-4">Hello from modal!</p>
   </b-modal>
+  
+    <b-container>
+
 
   </div>
 
