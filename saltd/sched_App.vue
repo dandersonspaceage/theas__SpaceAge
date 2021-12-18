801f304bbf5f7d3c573cd08fab985c470196c9cb
DavidRueter <drueter@opsstream.com>
2021-12-18 10:10:40 -0800

    WIP

--- saltd/sched_App.vue
+++ saltd/sched_App.vue
@@ -11,7 +11,7 @@
 
         <b-col>
 
-<select>
+<select :value="curWOList" @model="switchWOList()">
   <option v-for="option in woListNames" :key="option" :value="option">
     [[ option]]
   </option>
