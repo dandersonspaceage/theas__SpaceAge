e1732f4facaef0eed187f0ad8fc6bc3c2d19d4a3
DavidRueter <drueter@opsstream.com>
2021-12-20 19:07:59 -0800

    WIP

--- saltd/sched_App.vue
+++ saltd/sched_App.vue
@@ -183,7 +183,7 @@
 
     <b-container>
 
-    <b-modal id="thModal" ref="thModal" hide-footer>
+    <b-modal id="thModal" ref="thModal" v-model="showModal" hide-footer>
       <template #modal-title>
         Using <code>$bvModal</code> Methods
       </template>
