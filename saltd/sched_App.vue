be3af7285c152a0086fff80006fdc4c461ccf275
DavidRueter <drueter@opsstream.com>
2021-12-21 17:44:37 -0800

    WIP

--- saltd/sched_App.vue
+++ saltd/sched_App.vue
@@ -25,7 +25,7 @@
             <h6>[[ curWOList ]] (<span v-if="busy">Loading</span><span v-if="!busy">[[ data_WOs.length ]]</span><span>orders</span>)</h6>
               <div class="fastscroll" style="height:75vh;">
 
-                <draggable v-model="data_WOs" group="wo" filter="isDragEnabled()" handle=".dragHandle" @start="drag=true" @end="drag=false" @change="onDropWO">
+                <draggable v-model="data_WOs" group="wo" filter="isDragEnabled" handle=".dragHandle" @start="drag=true" @end="drag=false" @change="onDropWO">
 
                   <b-card v-for="wo in data_WOs" :key="wo.qguid">
 
