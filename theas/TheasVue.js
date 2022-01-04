6271ff86fa349a3c8c98d49c4b4f389c552724af
DavidRueter <drueter@opsstream.com>
2022-01-04 08:11:55 -0800

    WIP

--- theas/TheasVue.js
+++ theas/TheasVue.js
@@ -79,10 +79,10 @@ Theas.prototype.setVue = function (vue){
           let msgnodes = [];
 
           if (thatTheas.lastError.msgFriendly) {
-            msgnodes.push(thatTheas.thatVue.createElement('p', {}, msgFriendly));
+            msgnodes.push(thatTheas.thatVue.createElement('p', {}, thatTheas.lastError.msgFriendly));
           }
 
-          msgnodes.push(thatTheas.thatVue.createElement('i', {}, msgTech));
+          msgnodes.push(thatTheas.thatVue.createElement('i', {}, thatTheas.lastError.msgTech));
 
           this.showMessage(msgnodes, thatTheas.lastError.msgTitle);
         }    
