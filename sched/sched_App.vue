e770dc82862b0791b25cd5a7d6384f16ca973bc0
DavidRueter <drueter@opsstream.com>
2022-01-17 09:12:48 -0800

    WIP

--- sched/sched_App.vue
+++ sched/sched_App.vue
@@ -193,7 +193,7 @@
                         
                       </b-col>
 
-                      <b-col cols="1">
+                      <b-col cols="1" style="height:100%">
                         <b-row>
                           <b-col>
                             <div @click="clickBookmark(wo.qguid, $event)" @mousedown="mousedownBookmark" @mouseup.stop.prevent="mouseupBookmark" class="bookmark" :class="{ bookmark1: ('1'==wo.BookmarkCode), bookmark2: ('2'==wo.BookmarkCode), bookmark3: ('3'==wo.BookmarkCode) }"></div>                                       
