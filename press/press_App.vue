b8ea1e1cb5cbb939546446ff30a3210e2365d321
DavidRueter <drueter@opsstream.com>
2022-01-18 11:23:54 -0800

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -118,7 +118,7 @@
 {title: 'SO', field: 'LinkedSONumber'},
 {title: 'Qty', field: 'Quantity'},
 {title: 'QtyShot', field: 'CurrentShotCount'},
-{title: 'CommitDate', field: 'formatDate(wo.CommitDate, "MM/DD")'},
+{title: 'CommitDate', field: 'formatDate(CommitDate, "MM/DD")'},
 {title: 'WO', field: 'ItemNumber'},
 {title: 'Item', field: 'WONumber'},
 {title: 'Customer', field: 'CustomerName'},
