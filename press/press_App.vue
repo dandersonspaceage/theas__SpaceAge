984b1905eb7527561c6ebbc6e260b0d7c470dafb
DavidRueter <drueter@opsstream.com>
2022-01-19 08:33:04 -0800

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -30,12 +30,12 @@
 
     <b-row style="height: 20%; background-color: #BDB76B">
 
-        <b-col>
+        <b-col cols="2">
           <h3>Working on: xxxx  5 of 28</h3>
           <b-button size="lg" variant="success">Completed Board</b-button>
         </b-col>
 
-        <b-col cols="10">
+        <b-col>
           Another col
         </b-col>
 
