44ac6cb47983e4695d953d8c0ed0357a9d6289d6
DavidRueter <drueter@opsstream.com>
2022-01-19 09:57:02 -0800

    WIP

--- press/press.js
+++ press/press.js
@@ -1,5 +1,11 @@
 import vueNewPage1 from "{{ '/press/press_App.vue'|theasResource }}"
 
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+
     Vue.config.productionTip = false
     Vue.use(BootstrapVue);
 
