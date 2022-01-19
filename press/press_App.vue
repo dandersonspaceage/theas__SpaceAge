88f0332d12c12d9c7d45284d5213b001e77ced8a
DavidRueter <drueter@opsstream.com>
2022-01-19 07:45:45 -0800

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -62,7 +62,7 @@
 
             tab1Options: {
               responsiveLayout: 'collapse', // enable responsive layouts
-              height: '100%',
+              height: '68vh',
               layout: 'fitDataFill',
               //virtualDOM: "true",
               responsiveLayoutCollapseStartOpen: false,
