1a34efe1d9965b8d0c19b37b1786b014a76ae82c
DavidRueter <drueter@opsstream.com>
2022-01-20 08:57:08 -0800

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -80,7 +80,8 @@
             tab1Options: {
               responsiveLayout: 'collapse', // enable responsive layouts
               height: '68vh', //with responsiveCollapse we need to specify an absolute height
-              layout: 'fitDataFill',
+              //layout: 'fitDataFill',
+              layout: 'fitColumns',
               responsiveLayoutCollapseStartOpen: false,
               resizableColumns: true,
 
