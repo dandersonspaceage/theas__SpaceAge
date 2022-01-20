222a588ec74ea0a73411d581ac34257cea02bcd0
DavidRueter <drueter@opsstream.com>
2022-01-20 11:47:36 -0800

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -97,7 +97,7 @@
                   formatterParams: {formatStr: "dd MM/DD"}
                 },
 
-
+                {title: 'Thick OP', field: 'DimThickness_Finished'},
                 {title: 'Thick OP', field: 'DimThickness_OffPress'},
                 {title: 'Width OP', field: 'DimWidth_OffPress'},
                 {title: 'Length OP', field: 'DimLength_OffPress'},
