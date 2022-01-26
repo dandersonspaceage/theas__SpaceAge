f107147fc7d653a480c579862940caac74823765
DavidRueter <drueter@opsstream.com>
2022-01-26 07:44:08 -0800

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -43,13 +43,13 @@
     <b-row style="height:20%; background-color: #BDB76B">
 
         <b-col cols="6">
-          <h4>Table A</h4>
+          <h4>Table 1</h4>
           <h6>Working on: WO [[ data_ThisWOA.WONumber ]] (#[[ data_ThisWOA.CurrentShotCount + 1 ]] of [[ data_ThisWOA.Quantity]])</h6>
           <b-button @click="completeBoard" variant="primary">Completed Board</b-button>
         </b-col>
 
         <b-col>
-          <h4>Table B</h4>
+          <h4>Table 2</h4>
           <h6>Working on: WO [[ data_ThisWOB.WONumber ]] (#[[ data_ThisWOB.CurrentShotCount + 1 ]] of [[ data_ThisWOB.Quantity]])</h6>
           <b-button @click="completeBoard" variant="warning">Completed Board</b-button>
         </b-col>
