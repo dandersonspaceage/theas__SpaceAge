5f9ed4aed3187b5414c1b82d3a04d0b7dccee0d1
DavidRueter <drueter@opsstream.com>
2022-01-28 08:54:53 -0800

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -52,7 +52,7 @@
             </b-col>
 
             <b-col>     
-              <h6 style="font-size:calc(0.6em + 1.4vmin)">WO [[ data_ThisWOA.WONumber ]] (#[[ Number(data_ThisWOA.CurrentShotCount) + 1 ]] of [[ data_ThisWOA.Quantity]])</h6>
+              <h6 style="font-size:calc(0.6em + 1.4vmin)">WO [[ data_ThisWOA.WONumber ]] <span style="font-size:calc(0.6em + 1vmin)">#[[ Number(data_ThisWOA.CurrentShotCount) + 1 ]] of [[ data_ThisWOA.Quantity]]</span></h6>
             </b-col>      
           </b-row>      
 
@@ -87,7 +87,7 @@
             </b-col>
 
             <b-col>     
-              <h6 style="font-size:calc(0.6em + 1.4vmin)">WO [[ data_ThisWOB.WONumber ]] (#[[ Number(data_ThisWOB.CurrentShotCount) + 1 ]] of [[ data_ThisWOB.Quantity]])</h6>
+              <h6 style="font-size:calc(0.6em + 1.4vmin)">WO [[ data_ThisWOB.WONumber ]] <span style="font-size:calc(0.6em + 1vmin)">#[[ Number(data_ThisWOB.CurrentShotCount) + 1 ]] of [[ data_ThisWOB.Quantity]]</span></h6>
             </b-col>      
           </b-row>      
 
