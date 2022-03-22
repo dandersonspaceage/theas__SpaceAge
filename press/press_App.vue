3976a9569f1239fde3129fb333196959b0776207
DavidRueter <drueter@opsstream.com>
2022-03-22 08:39:43 -0700

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -167,7 +167,7 @@
   </b-modal>
 
 
-  <b-modal id="ShotHistoryModal" ref="ShotHistoryModal" @show="onShowShotHistory" @hide="onHideShotHistory($event)" hide-footer>
+  <b-modal id="ShotHistoryModal" ref="ShotHistoryModal" size="lg" @show="onShowShotHistory" @hide="onHideShotHistory($event)" hide-footer>
     <template #modal-title>
       <h4>Shot History</h4>
     </template>
