fb437a5aa27eab402b7b4cba066c9ef01eea7609
DavidRueter <drueter@opsstream.com>
2022-04-06 07:20:49 -0700

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -368,7 +368,7 @@
             tabShotHistOpt: {
 
               responsiveLayout: 'hide', // enable responsive layouts
-              height: '40%', //with responsiveCollapse we need to specify an absolute height
+              height: '40vh', //with responsiveCollapse we need to specify an absolute height
               layout: 'fitDataFill',
               //layout: 'fitColumns',
               //responsiveLayoutCollapseStartOpen: false,
