98dcbc125729bc069e9ef956d75cb9cf7dc04e08
DavidRueter <drueter@opsstream.com>
2022-04-21 05:04:21 -0700

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -664,6 +664,11 @@
         // save reference to Vue object
         let thatVue = this;
 
+        //for now, force a full refresh (not incremental) so that
+        //we can remove stale WOs
+        thatVue.lastFetch_WOs = null;
+        thatVue.data_WOs = [];
+
         thatVue.incBusy();        
 
         thatVue.$th.sendAsync({
