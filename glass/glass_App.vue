40460f1ccb02984b956a5fd0cb674169903fd609
DavidRueter <drueter@opsstream.com>
2022-05-23 05:59:36 -0700

    WIP

--- glass/glass_App.vue
+++ glass/glass_App.vue
@@ -79,7 +79,7 @@
 
             <b-col>     
               <h5 style="font-size:calc(0.6em + 1.4vmin)">WO [[ curWOTable1.WONumber ]]</h5>
-              <h6 style="font-size:calc(0.6em + 1vmin)">#[[ curWOTable1.GlassCutQuantity ]] of [[ curWOTable1.Quantity ]]</h6>
+              <h6 style="font-size:calc(0.6em + 1vmin)">Remaining to cut:  [[ curWOTable1.Quantity - curWOTable1.GlassCutQuantity ]] (of [[ curWOTable1.Quantity ]] total)</h6>
             </b-col>      
           </b-row>      
 
