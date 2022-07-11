0bba91e72cfe496dd49d87b9cc6c5e264ff32c29
DavidRueter <drueter@opsstream.com>
2022-07-11 15:51:23 -0700

    saveSelectWO

--- press/press_App.vue
+++ press/press_App.vue
@@ -790,14 +790,17 @@
                   thatVue.curWOListCode = thatVue.data_WOs[0].PlannedPress;
                 }
 
-                if (thatVue && thatVue.data_WOs && thatVue.data_WOs.length > 0) {
-                    thatVue.curWOTable1= thatVue.data_WOs[0];
+                if (thatVue && thatVue.data_WOs && thatVue.data_WOs.length > 0 && !thatVue.curWOqguid_Table1) {
+                    thatVue.curWOTable1 = thatVue.data_WOs[0];
                     thatVue.curWOTable1.TableNo = 'Table1';
-                    thatVue.curWOqguid_Table1 = thatVue.curWOTable1.qguid
-                    if (thatVue.data_WOs.length > 1) {
+                    thatVue.curWOqguid_Table1 = thatVue.curWOTable1.qguid                    
+                    thatVue.saveSelectWO(thatVue.curWOTable1.qguid, thatVue.curWOTable1.TableNo);              
+
+                    if (thatVue.data_WOs.length > 1 && !thatVue.curWOqguid_Table2) {
                       thatVue.curWOTable2= thatVue.data_WOs[1]; 
                       thatVue.curWOTable2.TableNo = 'Table2';
                       thatVue.curWOqguid_Table2 = thatVue.curWOTable2.qguid;                                           
+                      thatVue.saveSelectWO(thatVue.curWOTable2.qguid, thatVue.curWOTable2.TableNo);                       
                     }
                 }             
 
