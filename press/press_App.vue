92733e1a35e7b86fb312bc19b80d07acbacc2cd1
DavidRueter <drueter@opsstream.com>
2022-07-18 12:15:05 -0700

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -90,33 +90,25 @@
                   </b-form-select>                          
                 </b-form-group>
 
-<div>
-                <b-button @click="print" variant="success" size="sm" class="p-0 m-0 mt-1">Print Pallet Label</b-button>
+                <div>
+                  <b-button @click="print('printBoardLabel2')" variant="success" size="sm" class="p-0 m-0 mt-1">Print Pallet Label</b-button>
 
-                <div id="printMe" style="visibility: hidden">
+                  <div id="printBoardLabel2" style="visibility: hidden">
 
-                    <p style="text-align:center">
-                      <img width=221 height=136 id="Picture 1" src="/resources/spaceage.jpg" alt="SpaceAge">
-                    </p>
+                      <p style="text-align:center">
+                        <img width=221 height=136 id="Picture2" src="/resources/spaceage.jpg" alt="SpaceAge">
+                      </p>
 
-                  <h1 class="pb-5" style="text-align:center">Board Pallet Label</h1>
-                  
-                  <h2 class="py-4">WO #: <span class="font-weight-bold">[[ curWOTable1.WONumber ]]</span></h2>
-                  
-                  <h2 class="py-4">Customer: <span class="font-weight-bold">[[ curWOTable1.CustomerName ]]</span></h2>
-
-                  <h2 class="py-4">Description: <span class="font-weight-bold">[[ curWOTable1.GlassSpecs ]]</span></h2>
-                  
-                  <h2 class="py-4">Off-Press Dimensions: <span class="font-weight-bold">[[ curWOTable1.DimThickness_OffPress ]] X [[ curWOTable1.DimWidth_OffPress ]] X [[ curWOTable1.DimLength_OffPress ]]</span></h2>
-                                
-                  <h2 class="py-4">Quantity: <span class="font-weight-bold">[[ printQuantity ]]</span></h2>
-                              
-                  <h2 class="py-4">Cut By: <span class="font-weight-bold">[[ curWorkerAbbrev]]</span></h2>                         
-                  
-                  <h2 class="py-4">Date: <span class="font-weight-bold">[[ curDate ]]</span></h2>                      
-
-                </div>  
-</div>                
+                    <h1 class="pb-5" style="text-align:center">Board Pallet Label</h1>                  
+                    <h2 class="py-4">WO #: <span class="font-weight-bold">[[ curWOTable2.WONumber ]]</span></h2>                  
+                    <h2 class="py-4">Customer: <span class="font-weight-bold">[[ curWOTable2.CustomerName ]]</span></h2>
+                    <h2 class="py-4">Description: <span class="font-weight-bold">[[ curWOTable2.GlassSpecs ]]</span></h2>                  
+                    <h2 class="py-4">Off-Press Dimensions: <span class="font-weight-bold">[[ curWOTable2.DimThickness_OffPress ]] X [[ curWOTable2.DimWidth_OffPress ]] X [[ curWOTable2.DimLength_OffPress ]]</span></h2>                                
+                    <h2 class="py-4">Quantity: <span class="font-weight-bold">[[ printQuantity ]]</span></h2>                              
+                    <h2 class="py-4">Cut By: <span class="font-weight-bold">[[ curWorkerAbbrev]]</span></h2>                                           
+                    <h2 class="py-4">Date: <span class="font-weight-bold">[[ curDate ]]</span></h2>                      
+                  </div>  
+                </div>                
 
               </b-col>
             </b-row>
@@ -150,20 +142,40 @@
               <b-col>
                 <h4 class="p-0 m-0" style="font-size:calc(1em + .4vmin)">Table&nbsp;1</h4>
 
-                  <b-form-group label="Active WO"
-                              :label-for="'woTable1'"
-                              style="max-width: 150px"
-                              class="p-0 m-0">
+                <b-form-group label="Active WO"
+                            :label-for="'woTable1'"
+                            style="max-width: 150px"
+                            class="p-0 m-0">
 
-                    <b-form-select :id="'woTable1'"
-                                  :options="data_WOs"
-                                  v-model="curWOqguid_Table1"
-                                  size="sm"
-                                  class="p-0 m-0"
-                                  @change="changeActiveWO('Table1', curWOqguid_Table1)"
-                                  >
-                    </b-form-select>                          
-                  </b-form-group>
+                  <b-form-select :id="'woTable1'"
+                                :options="data_WOs"
+                                v-model="curWOqguid_Table1"
+                                size="sm"
+                                class="p-0 m-0"
+                                @change="changeActiveWO('Table1', curWOqguid_Table1)"
+                                >
+                  </b-form-select>                          
+                </b-form-group>
+
+                <div>
+                  <b-button @click="print('printBoardLabel1')" variant="success" size="sm" class="p-0 m-0 mt-1">Print Pallet Label</b-button>
+
+                  <div id="printBoardLabe1" style="visibility: hidden">
+
+                      <p style="text-align:center">
+                        <img width=221 height=136 id="Picture1" src="/resources/spaceage.jpg" alt="SpaceAge">
+                      </p>
+
+                    <h1 class="pb-5" style="text-align:center">Board Pallet Label</h1>                  
+                    <h2 class="py-4">WO #: <span class="font-weight-bold">[[ curWOTable1.WONumber ]]</span></h2>                  
+                    <h2 class="py-4">Customer: <span class="font-weight-bold">[[ curWOTable1.CustomerName ]]</span></h2>
+                    <h2 class="py-4">Description: <span class="font-weight-bold">[[ curWOTable1.GlassSpecs ]]</span></h2>                  
+                    <h2 class="py-4">Off-Press Dimensions: <span class="font-weight-bold">[[ curWOTable1.DimThickness_OffPress ]] X [[ curWOTable1.DimWidth_OffPress ]] X [[ curWOTable1.DimLength_OffPress ]]</span></h2>                                
+                    <h2 class="py-4">Quantity: <span class="font-weight-bold">[[ printQuantity ]]</span></h2>                              
+                    <h2 class="py-4">Cut By: <span class="font-weight-bold">[[ curWorkerAbbrev]]</span></h2>                                           
+                    <h2 class="py-4">Date: <span class="font-weight-bold">[[ curDate ]]</span></h2>                      
+                  </div>  
+                </div>                    
 
               </b-col>
 
@@ -606,7 +618,7 @@
       }, 
 
 
-      async print () {
+      async print (whatDiv) {
         let thatVue = this;
 
         if (!thatVue.curWorkerQGUID) {
@@ -615,11 +627,8 @@
         }        
         
         else {
-
-          thatVue.curWO = thatVue.curWOTable1;
-
           // Pass the element id here
-          await this.$htmlToPaper('printMe');
+          await this.$htmlToPaper(whatDiv);
         }
       },
 
