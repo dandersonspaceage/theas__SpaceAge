5bfa2fa4e5764bd2b6f08368ae6a2ea07d5e8524
DavidRueter <drueter@opsstream.com>
2022-07-25 09:54:29 -0700

    WIP

--- press/press_App.vue
+++ press/press_App.vue
@@ -926,18 +926,26 @@
 
                 //apply server-side WO table assignments
                 if (thatVue && thatVue.data_WOs && thatVue.data_WOs.length > 0) {
-                    thatVue.curWOTable1 = thatVue.data_WOs.find((el) => el.ActiveTable1 === '1');
+                    let thisIndex1 = thatVue.data_WOs.findIndex((el) => el.ActiveTable1 === '1');
+                    if (thisIndex1 >= 0) {
+                      thatVue.curWOTable1 = thatVue.data_WOs[thisIndex1];
+                    }
+                    else {
+                      thatVue.curWOTable1 = {};
+                    }
                     thatVue.curWOqguid_Table1 = null;
                     if (thatVue.curWOTable1) {
                       thatVue.curWOqguid_Table1 = thatVue.curWOTable1.qguid;                       
                     }
                           
                     if (thatVue.data_WOs.length > 1) {
-                      thatVue.curWOTable2 = thatVue.data_WOs.find((el) => el.ActiveTable2 === '2');
-                      thatVue.curWOqguid_Table2 = null;                      
-                      if (thatVue.curWOTable2) {
-                        thatVue.curWOqguid_Table2 = thatVue.curWOTable2.qguid;   
-                      }                                                           
+                      let thisIndex2 = thatVue.data_WOs.findIndex((el) => el.ActiveTable2 === '2');                    
+                      if (thisIndex2 >= 0) {
+                        thatVue.curWOTable2 = thatVue.data_WOs[thisIndex2];
+                      }
+                      else {
+                        thatVue.curWOTable2 = {};
+                      }                                                                              
                     }
                 }             
                 
