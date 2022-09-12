29d603fb06ef87ce1b14539bebb681206aa119a3
DavidRueter <drueter@opsstream.com>
2022-09-12 14:08:45 -0700

    WIP

--- vue/vueSAOps.vue
+++ vue/vueSAOps.vue
@@ -88,6 +88,8 @@
                                   class="p-0 m-0"
                                   @change="changeActiveWO('Table2', curWOqguid_Table2)"
                                   >
+  
+                        <b-form-select-option value=null>No Selection</b-form-select-option>                                
                     </b-form-select>                          
                   </b-form-group>
   
@@ -148,6 +150,8 @@
                                   class="p-0 m-0"
                                   @change="changeActiveWO('Table1', curWOqguid_Table1)"
                                   >
+  
+                                  <b-form-select-option value=null>No Selection</b-form-select-option>                                
                     </b-form-select>                          
                   </b-form-group>
   
@@ -893,6 +897,71 @@
           thatVue.fetchData();
         },
   
+        setWOSelections: function(set_from_array) {
+          //apply server-side WO table assignments             
+  
+          let thisIndex1 = null;
+          let thisIndex2 = null;
+            
+            
+          if (set_from_array) {
+  
+            // set selections from data_WOs[].ActiveTable1 and .ActiveTable2
+  
+            if (thatVue.data_WOs && thatVue.data_WOs.length > 0) {
+              // have WorkOrders.  Try to set selections
+              thisIndex1 = thatVue.data_WOs.findIndex((el) => el.ActiveTable1 === '1');
+              thisIndex2 = thatVue.data_WOs.findIndex((el) => el.ActiveTable2 === '2');
+            }
+          }
+  
+          else {
+            //set selections from curWOqguid_Table1 and Table2 which was set by the select control
+            thisIndex1 = thatVue.data_WOs.findIndex((el) => el.qugid === curWOqguid_Table1); 
+            thisIndex2 = thatVue.data_WOs.findIndex((el) => el.qugid === curWOqguid_Table2);                 
+          }
+  
+  
+          // set thatVue.curWOTable1 and thatVue.curWOTable2 objects
+          if (thisIndex1 >= 0) {
+            thatVue.$set(thatVue, 'curWOTable1', thatVue.data_WOs[thisIndex1]);                                            
+          }
+          else {
+            //active WO for Table1 not found
+            thatVue.$set(thatVue, 'curWOTable1', {});                     
+          }       
+                    
+          if (thisIndex2 >= 0) {
+            thatVue.$set(thatVue, 'curWOTable2', thatVue.data_WOs[thisIndex2]);                        
+          }
+          else {
+            //active WO for Table2 not found                        
+            thatVue.$set(thatVue, 'curWOTable2', {});             
+          }
+                         
+  
+          if (set_from_array){
+  
+            if (thatVue.curWOTable1 && thatVue.curWOTable1.qguid) {
+              thatVue.curWOqguid_Table1 = thatVue.curWOTable1.qguid;                
+            }
+            else {
+              thatVue.curWOqguid_Table1 = null   
+              thatVue.$set(thatVue, 'curWOTable1', {});            
+            }
+  
+            if (thatVue.curWOTable2 && thatVue.curWOTable2.qguid) {
+              thatVue.curWOqguid_Table2 = thatVue.curWOTable2.qguid;                
+            }
+            else {
+              thatVue.curWOqguid_Table2 = null   
+              thatVue.$set(thatVue, 'curWOTable2', {});              
+            }              
+  
+          }      
+            
+        },
+  
         fetchWorkers: function (workerType) {
           // save reference to Vue object
           let thatVue = this;
@@ -1018,6 +1087,12 @@
   
                 if (rd["General"]) {
                   thisObj = JSON.parse(rd["General"]);
+  
+                  if (thatVue.CurrentShift != thisObj.CurrentShift) {
+                    thatVue.curWorkerQGUID = null;  // a new shift, so clear out the current
+                    thatVue.CurrentShift = thisObj.CurrentShift;                 
+                  }
+  
                   if (thisObj.PressCode) {
                     thatVue.lockPressSelection = true;
                     thatVue.curWOListCode = thisObj.PressCode;
@@ -1079,36 +1154,7 @@
                   }
   
   
-                  //apply server-side WO table assignments
-                  if (thatVue && thatVue.data_WOs && thatVue.data_WOs.length > 0) {
-                      let thisIndex1 = thatVue.data_WOs.findIndex((el) => el.ActiveTable1 === '1');
-                      if (thisIndex1 >= 0) {
-                        thatVue.curWOTable1 = thatVue.data_WOs[thisIndex1];
-                      }
-                      else {
-                        thatVue.curWOTable1 = {};
-                      }
-                      thatVue.curWOqguid_Table1 = null;
-                      if (thatVue.curWOTable1.qguid) {
-                        thatVue.curWOqguid_Table1 = thatVue.curWOTable1.qguid;                       
-                      }
-                            
-                      if (thatVue.data_WOs.length > 1) {
-                        let thisIndex2 = thatVue.data_WOs.findIndex((el) => el.ActiveTable2 === '2');                    
-                        if (thisIndex2 >= 0) {
-                          thatVue.curWOTable2 = thatVue.data_WOs[thisIndex2];
-                        }
-                        else {
-                          thatVue.curWOTable2 = {};
-                        }                                                                              
-                      }
-  
-                      thatVue.curWOqguid_Table2 = null;
-                      if (thatVue.curWOTable2.qguid) {
-                        thatVue.curWOqguid_Table2 = thatVue.curWOTable2.qguid;                       
-                      }
-  
-                  }                             
+                  thatVue.setWOSelections(true);       
   
                 }
   
@@ -1258,24 +1304,21 @@
         changeActiveWO: function (tableCode, qguid) {
           let thatVue = this; 
   
-          let wo = thatVue.data_WOs.find((el) => el.qguid === qguid);
-  
           if (tableCode == 'Table1') {
-            if (thatVue.curWOTable1 != wo) {
-              thatVue.curWOTable1 = wo;
-  
+            if (thatVue.curWOqguid_Table1 != qguid) {
+              thatVue.curWOqguid_Table1 = qguid;
               thatVue.saveSelectWO(qguid, tableCode);
             }
           }
   
           else if (tableCode == 'Table2') {
-            if (thatVue.curWOTable2 != wo) {          
-              thatVue.curWOTable2 = wo;    
-  
+            if (thatVue.curWOqguid_Table2 != qguid) {
+              thatVue.curWOqguid_Table2 = qguid;
               thatVue.saveSelectWO(qguid, tableCode);
-            }          
+            }        
           }
-  
+              
+          thatVue.setWOSelections(false);
         },
   
         showHistory: function() {        
