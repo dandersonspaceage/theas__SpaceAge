2fac659fa94bf8078dc69cae25a31edd24eaa9d9
DavidRueter <drueter@opsstream.com>
2022-10-29 08:01:26 -0700

    WIP

--- theas/templates/vue_blank/blankvue.js
+++ theas/templates/vue_blank/blankvue.js
@@ -1,4 +1,4 @@
-import vueSAOps from "{{ 'blankvue.vue'|theasResource }}"
+import vueSAOps from "{{ './blankvue.vue'|theasResource }}"
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
