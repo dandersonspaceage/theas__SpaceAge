f45eb2c2a69e5c954a4b46d8cb3a405aceac6bc3
DavidRueter <drueter@opsstream.com>
2022-10-29 07:20:35 -0700

    cleanup: remove unused files

--- glass/glass.js
+++ glass/glass.js
@@ -1,4 +1,4 @@
-import vueNewPage1 from "{{ '/glass/glass_App.vue'|theasResource }}"
+import vueNewPage1 from "{{ '/vue/vueSAOps.vue'|theasResource }}"
 
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;
