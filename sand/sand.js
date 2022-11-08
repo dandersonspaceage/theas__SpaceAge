fb5c5d04ca75dba4f05c0e6287e193d0ccbaf345
DavidRueter <drueter@opsstream.com>
2022-11-08 07:59:41 -0800

    Sanding screen WIP

--- sand/sand.js
+++ sand/sand.js
@@ -1,4 +1,4 @@
-import vueSAOps from "{{ '/vue/vueSAOps.vue'|theasResource }}"
+import vueSAOps from "{{ '/sand/vueSAOps.vue'|theasResource }}"
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
